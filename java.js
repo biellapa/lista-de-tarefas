6ba0ca9ae1098b3de01e43ea14ecc9af05489868
gabriel cardoso da lapa <98766972+biellapa@users.noreply.github.com>
2022-05-30 15:27:28 -0300

    .

--- java.js
+++ java.js
@@ -1,1 +1,26 @@
-alert('ola')
+const input = document.getElementById("input")
+const lista = document.getElementById("lista")
+const tarefas = []
+
+function add() {
+    if(input.value != "")
+    tarefas.push(input.value)
+    input.value = ""
+    render()
+}
+function render() {
+    lista.innerHTML = null
+    tarefas.forEach(function (tarefa) {
+        const li  = document.createElement("li")
+        li.innerText = tarefa
+        lista.appendChild(li)
+        const button = document.createElement("button")
+        button.innerHTML = "X"
+        button.addEventListener("click", function() {
+            const index = tarefas.indexOf(tarefa)
+            tarefas.splice(index, 1)
+            render()
+        })
+        li.appendChild(button)
+    })
+}
